9d1098b64a44910ceb3886efa43ed21fa398d8ac
Yigit Nuhuz <yigit.nuhuz@ackare.com>
2019-01-12 14:39:00 +0300

    project files

---
+++ dotnetcore-raspbian-console-app/CaylakConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaylakConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raspberry Pi'dan merhaba!");
+        }
+    }
+}

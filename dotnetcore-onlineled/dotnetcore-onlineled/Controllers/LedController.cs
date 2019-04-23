e4bd8182323777b778c60236c3f26fef998ff7b4
Yigit Nuhuz <yigitnuhuz@gmail.com>
2019-04-23 19:15:31 +0300

    .net core mvc online led yakma projesi eklendi.

---
+++ dotnetcore-onlineled/dotnetcore-onlineled/Controllers/LedController.cs
@@ -0,0 +1,45 @@
+﻿using System.Device.Gpio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_onlineled.Controllers
+{
+    public class LedController : Controller
+    {
+        //led'i bağladığımız pin numarasını veriyoruz. 
+        private const int LedPin = 17;
+        private readonly GpioController _gpioController;
+
+        //constructor tanımlayıp burada pin yöneticimizi ve kullanacağımız pini tanımlıyoruz
+        public LedController()
+        {
+            _gpioController = new GpioController();
+            //verilen pini çıktı verecek şekilde tanımlıyoruz.
+            _gpioController.OpenPin(LedPin, PinMode.Output);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LedAc()
+        {
+            //verilmiş olan pini voltaj verecek şekilde değiştiriyoruz
+            _gpioController.Write(LedPin, PinValue.High);
+            //kullanıcıya bilgi vermek için bir mesaj yazıyoruz.
+            ViewBag.LedDurumu = "Led Açık!";
+
+            return View("Index");
+        }
+
+        public IActionResult LedKapat()
+        {
+            //verilmiş olan pini voltajı kesecek şekilde değiştiriyoruz
+            _gpioController.Write(LedPin, PinValue.Low);
+            //kullanıcıya bilgi vermek için bir mesaj yazıyoruz.
+            ViewBag.LedDurumu = "Led Kapalı!";
+
+            return View("Index");
+        }
+    }
+}
---
+++ dotnetcore-onlineled/dotnetcore-onlineled/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dotnetcore_onlineled
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Led}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
